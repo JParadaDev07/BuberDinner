987b2f27335aa50e2d6fb6bd697762069d4c8019
Julian Parada <alexgualteros02@gmail.com>
2024-05-28 11:55:14 -0500

    refactor: Update AuthenticationController to use MediatR for user registration and login

--- BuberDinner.Api/Controllers/ErrorsController.cs
+++ BuberDinner.Api/Controllers/ErrorsController.cs
@@ -8,15 +8,6 @@ public class ErrorsController : ControllerBase
     [Route("/error")]
     public IActionResult Error()
     {
-        // Exception? exception = HttpContext.Features.Get<IExceptionHandlerFeature>()?.Error;
-
-        // var (statusCode, message) = exception switch
-        // {
-        //     IError serviceException => ((int)serviceException.StatusCode, serviceException.ErrorMessage),
-        //     _ => (StatusCodes.Status500InternalServerError, "An error occurred while processing your request.")
-        
-        // };
-        
         return Problem();
     }
 
